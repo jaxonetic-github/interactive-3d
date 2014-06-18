35ecc14c9992012dcac6ae6a15f2bf54bcaa7e30
jaxonetic <jaxonetic@gmail.com>
2014-06-18 15:43:44 -0300

    fixed chat pagination **>> Learned that you can't set Session variable from within a Template helper; good to know :-)

--- client/views/chat/chat.js
+++ client/views/chat/chat.js
@@ -1,45 +1,66 @@
-Session.setDefault("chatPostsCursor",0);
+var PAGE_SIZE = 6;
+Session.setDefault("PostsCursor",0);
 
-Meteor.autorun(function(){
-	Meteor.subscribe("chatP osts", Session.get('chatPostsCursor'))
+Deps.autorun(function(){
+	Meteor.subscribe("ChatHistory", Session.get('PostsCursor'));	
 })
-
-
-Template.chat_input.nextText = function(){
-	return (Number(Session.get(chatPostsCursor))+20) +" - " + (Number(Session.get(chatPostsCursor))+40) ;
-}
-Template.chat_input.previousText = function(){
-	if(Number(Session.get(chatPostsCursor))<20){
-		return "";
-	}
-	return (Number(Session.get(chatPostsCursor))-20) +" - " + (Number(Session.get(chatPostsCursor))) ;
-}
+		
 
 
 Template.chat_input.events({
 	'submit #chatpost-form': function(e){
 		e.preventDefault();
-		
+		console.log("SUBMITTING");
 		var chatText = $('#posttext').val(); 
 		
 		if(chatText.length){
 			Meteor.call('submitChatPost', chatText);
+			$('#posttext').val(""); 
 		}
-	},
+	}});
+	
+Template.chat_history.events({
 	
 	'click .previous ': function(evt, tmpl){
-		if(Number(Session.get("chatPostsCursor")) >6 ){
-			Session.set("chatPostsCursor", Number(Session.get("chatPostsCursor"))-6)
+		console.log("previous");
+		console.log("chatcursor@=",Session.get("PostsCursor"));
+		if(Number(Session.get("PostsCursor")) >=PAGE_SIZE ){
+			Session.set("PostsCursor", Number(Session.get("PostsCursor"))-PAGE_SIZE)
 		}
+		console.log("chatcursor@=",Session.get("PostsCursor"));
 	},
 	
 	'click .next ': function(evt, tmpl){
-			Session.set("chatPostsCursor", Number(Session.get("chatPostsCursor"))+6)
+		console.log("next");
+		
+			Session.set("PostsCursor", Number(Session.get("PostsCursor"))+PAGE_SIZE);
+			console.log("chatcursor@=",Session.get("PostsCursor"));
 	}
 	
 	
 })
+Template.chat_history.helpers({
+	nextText : function(){
+	var tmp = Number(Session.get("PostsCursor"))+PAGE_SIZE ;
+	console.log(tmp);
+	//Session.set("PostsCursor",tmp);
+	return (Number(Session.get("PostsCursor"))+PAGE_SIZE) +" - " + (Number(Session.get("PostsCursor"))+12) ;
+},
+	previousText : function(){
+	console.log(Session.get("PostsCursor"));
+	if(Number(Session.get("PostsCursor"))<PAGE_SIZE){
+		return "";
+	}
+	//Session.set("PostsCursor",(Number(Session.get("PostsCursor"))-PAGE_SIZE)); 
+	return (Number(Session.get("PostsCursor"))-PAGE_SIZE+1) +" - " + (Number(Session.get("PostsCursor"))) ;
+},
 
-Template.chat_history.chat_posts= function(){
-	return ChatPosts.find();
+chatHistory: function(){
+	
+	console.log("inside chat_posts",ChatHistory.find().count());
+		//Meteor.subscribe("ChatHistory", Session.get('PostsCursor'));
+	//console.log();
+	 return ChatHistory.find();
 }
+})
+
--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
 Meteor.methods({
 	'submitChatPost': function(chatText){
-						console.log(chatText);
-						ChatPosts.insert({chatText:chatText, date:Date.now});
+						console.log("chatter chatted"+chatText);
+						ChatHistory.insert({chatText:chatText, date:Date.now});
+						console.log(ChatHistory.find().count());
 	},
 	'submitBlogPost': function(title, body){
 						Blogs.insert({title:title, body:body, date:Date.now});
 	},
 	'submitReferencePost': function(title,url, description){
 					 	References.insert({title:title,url:url, description:description, date:Date.now});
-	},
-	'submitMultiChatSession': function(session_id){
-		 RTCSessions.insert({session_id:session_id, numUsersInSession:1, date:Date.now});		
 	}
 })
 
-Meteor.publish("chatPosts", function(chatPostsCursor){
-	return ChatPosts.find({}, {limit:6, skip:chatPostsCursor});
+Meteor.publish("ChatHistory", function(chatPostsCursor){
+	return ChatHistory.find({}, {limit:6, skip:chatPostsCursor});
 })
